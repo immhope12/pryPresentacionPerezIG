7e664bd7f77f9aa289e38027bb6de097a203aa52
Alumno <Alumno@PC185>
2026-08-12 08:39:30 -0300

    Agregar archivos de proyecto.

---
+++ pryPresentacionPerezIG/Form1.cs
@@ -0,0 +1,10 @@
+namespace pryPresentacionPerezIG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ pryPresentacionPerezIG/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace pryPresentacionPerezIG
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
